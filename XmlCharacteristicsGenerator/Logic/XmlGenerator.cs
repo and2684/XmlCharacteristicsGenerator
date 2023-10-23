bb5d74cbd2810bd9798b6558849e9b0343a00b2d
and2684 <aaaa6226@gmail.com>
2023-10-23 10:55:28 +0500

    Убрал синхронную версию SaveToXml

--- XmlCharacteristicsGenerator/Logic/XmlGenerator.cs
+++ XmlCharacteristicsGenerator/Logic/XmlGenerator.cs
@@ -42,32 +42,5 @@ namespace XmlCharacteristicsGenerator.Logic
             // Записываем в файл
             await File.WriteAllTextAsync(fileName, xmlWriter.ToString(), windows1251);
         }
-
-
-        public static void SaveToXmlFile(this XmlInfoString xmlInfoString)
-        {
-            var element = new XElement("objects",
-                new XElement("object",
-                    new XAttribute("action", "INSERT"),
-                    new XAttribute("pipeto", "cat_send"),
-                    new XElement("obj_name", "RESHAR"),
-                    new XElement("fields",
-                        new XElement("ID", ""),
-                        new XElement("RESUN_ID", xmlInfoString.ResunId),
-                        new XElement("TYPEDESCR", xmlInfoString.Typedescr),
-                        new XElement("VALUE", xmlInfoString.Value),
-                        new XElement("VALUETO", ""),
-                        new XElement("UNIT_ID", xmlInfoString.UnitId),
-                        new XElement("FLAGREQUIRED", xmlInfoString.FlagRequired)
-                    ),
-                    new XElement("spec", " ")
-                )
-            );
-
-            var fileName = Path.Combine("C:\\HARS", $"{xmlInfoString.ResunId[..11]}.xml");
-
-            using var writer = new StreamWriter(fileName);
-            writer.Write(element.ToString());
-        }
     }
 }
